b2d9f7b9e5a97174bbadf3793f97483c1301d3d1
Ameer Mohammad <96792013+ameerM09@users.noreply.github.com>
2022-08-15 16:27:36 -0700

    Update menu_btn.py

--- settings/menu_btn.py
+++ settings/menu_btn.py
@@ -1,1 +1,25 @@
+from . import *
 
+class PlayButton():
+	def __init__(self, x, y, btn_image):
+		self.btn_image = MAIN_MENU_PLAY_BTN
+		self.rect = self.btn_image.get_rect()
+		self.rect.topleft = (x, y)
+		self.clicked = False
+
+	def check_for_click(self):
+		action = False
+		cursor_pos = pygame.mouse.get_pos()
+
+		if self.rect.collidepoint(cursor_pos):
+			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+				self.clicked = True
+				action = True
+
+			if pygame.mouse.get_pressed()[0] == 0:
+				self.clicked = False
+
+		return action
+
+	def draw(self, win):
+		win.blit(self.btn_image, (self.rect.x, self.rect.y))
